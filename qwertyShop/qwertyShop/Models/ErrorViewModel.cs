7f32a4002d765d6b060665498272558b30a2bd7e
Kuznetsova Kristina <101335982+smithy74@users.noreply.github.com>
2023-06-05 21:36:44 +0300

    update architecture
    using ASP.NET Core Wep App (Model-View-Controller)

---
+++ qwertyShop/qwertyShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace qwertyShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
